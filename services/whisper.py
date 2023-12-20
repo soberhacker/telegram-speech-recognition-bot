1f404cded1062fb24c0e36b57dbf586616e4523d
Burserver <128756825+soberhacker@users.noreply.github.com>
2023-12-20 12:24:14 +0200

    refactor

--- services/whisper.py
+++ services/whisper.py
@@ -4,24 +4,27 @@ from whisper.model import Whisper
 from whisper import load_model
 import config.botSettings as config
 
-def transcribe_whisper(model, file_path, language='ru'):    
+
+def transcribe_whisper(model, file_path, language="ru"):
     whisper_model: Whisper = model
-    logging.info('Audio recognition started (whisper)')    
-    result = whisper_model.transcribe(file_path, verbose=False, language = language)
-    rawtext = ' '.join([segment['text'].strip() for segment in result['segments']]) # type: ignore
+    logging.info("Audio recognition started (whisper)")
+    result = whisper_model.transcribe(file_path, verbose=False, language=language, fp16=False)
+    rawtext = " ".join([segment["text"].strip() for segment in result["segments"]])  # type: ignore
     rawtext = re.sub(" +", " ", rawtext)
     alltext = re.sub(r"([\.\!\?]) ", r"\1\n", rawtext)
-    logging.info(f'Recognized: {alltext}')
+    logging.info(f"Recognized: {alltext}")
     return alltext
 
+
 def load_whisper_model(lang_code) -> Whisper:
-    whisper_model = config.whisper_model + ('.' + lang_code if lang_code == 'en' else '' )
-    return load_model(whisper_model)        
+    whisper_model = config.whisper_model + ("." + lang_code if lang_code == "en" else "")
+    return load_model(whisper_model)
+
 
 """ # TODO: Add async transcribe_whisper and transcribe_vosk
 async def stt(audio_file_path) -> str:
     logging.info('Audio recognition started')
-    loop = asyncio.get_event_loop()    
+    loop = asyncio.get_event_loop()
     try:
         return await loop.run_in_executor(None, transcribe_whisper, audio_file_path)
     except:
